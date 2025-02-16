3dd99ca58acd683673325987ea688fbdef898cf5
Manjeet-24 <manjeet.8207@gmail.com>
2025-02-16 12:07:30 -0800

    minor updates

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 8000;
 const corsOptions = {
-    origin: "http://localhost:5173",
+    origin: "https://gharbazaar.netlify.app",
     credentials: true
 }
 
